b1e972836a202116d4c48ec8dbbb1c4aff0a88af
agiletkiewicz <a.letkiewicz@gmail.com>
2021-03-18 13:04:06 -0700

    Click movie cards respond to click

--- src/components/movie.js
+++ src/components/movie.js
@@ -1,18 +1,22 @@
 import Card from 'react-bootstrap/Card';
+import { useState } from 'react';
 
 function Movie({movie}) {
 
-    return (
-      <Card>
-        {movie.Poster === "N/A" ? null : <Card.Img src={movie.Poster} />}
-        <Card.Body>
-          <Card.Title>{movie.Title}</Card.Title>
-          <Card.Text>
-            {movie.Year}
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    );
-  }
+  const [clicked, setClicked] = useState(false);
+
+  return (
+    <Card onClick={() => setClicked(!clicked)} className={clicked ? "border-success" : null}>
+      {movie.Poster === "N/A" ? null : <Card.Img src={movie.Poster} />}
+      <Card.Body>
+        <Card.Title>{movie.Title}</Card.Title>
+        <Card.Text>
+          {movie.Year}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+  
+}
   
   export default Movie;
--- src/containers/movies.js
+++ src/containers/movies.js
@@ -2,13 +2,15 @@ import Movie from '../components/movie';
 import CardDeck from 'react-bootstrap/CardDeck'
 
 function Movies({ movies }) {
-    return (
-      <CardDeck>
-        {movies.map(movie => (
-          <Movie key={movie.imdbID} movie={movie} />
-        ))}
-      </CardDeck>
-    );
-  }
+
+  return (
+    <CardDeck>
+      {movies.map(movie => (
+        <Movie key={movie.imdbID} movie={movie}/>
+      ))}
+    </CardDeck>
+  );
+
+}
   
   export default Movies;
